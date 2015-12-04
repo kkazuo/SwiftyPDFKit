495c496e5d5e0104aa642de7ea7e3eac028f7bd2
Kazuo Koga <obiwanko@me.com>
2015-12-05 02:19:41 +0900

    Initial Commit

---
+++ SwiftyPDFKit/Dictionary.swift
@@ -0,0 +1,110 @@
+//
+//  Dictionary.swift
+//  SwiftyPDFKit
+//
+//  Created by Kazuo Koga on 2015/12/05.
+/*
+Copyright (c) 2015 Kazuo Koga
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+import Foundation
+import CoreGraphics
+
+extension CGPDFDictionaryRef {
+    
+    func shallowCopy() -> PDFDictionaryType {
+        let f: CGPDFDictionaryApplierFunction = { ckey, obj, ctx in
+            var dict = UnsafeMutablePointer<PDFDictionaryType>(ctx).memory
+            guard let key = String.fromCString(ckey) else {
+                return
+            }
+            switch CGPDFObjectGetType(obj) {
+            case .Null:
+                dict[key] = NSNull()
+                
+            case .Boolean:
+                var bool = false
+                bool = withUnsafeMutablePointer(&bool) { v in
+                    CGPDFObjectGetValue(obj, .Boolean, v)
+                    return v.memory
+                }
+                dict[key] = bool
+                
+            case .Integer: 0
+                
+            case .Real: 0
+                
+            case .Name:
+                var cc: CChar = 0
+                let s: String? = withUnsafeMutablePointer(&cc) { v in
+                    CGPDFObjectGetValue(obj, .Name, v)
+                    return String.fromCString(v)
+                }
+                if let s = s {
+                    dict[key] = s
+                }
+                
+            case .String:
+                var ps = CGPDFStringRef()
+                let s: AnyObject? = withUnsafeMutablePointer(&ps) { v in
+                    CGPDFObjectGetValue(obj, .String, v)
+                    if let cd = CGPDFStringCopyDate(v.memory) {
+                        return cd as NSDate
+                    }
+                    if let cs = CGPDFStringCopyTextString(v.memory) {
+                        return cs as String
+                    }
+                    return nil
+                }
+                if let s = s {
+                    dict[key] = s
+                }
+                
+            case .Array:
+                dict[key] = []
+                
+            case .Dictionary:
+                dict[key] = [:]
+                
+            case .Stream: 0
+            }
+            
+            UnsafeMutablePointer<PDFDictionaryType>(ctx).memory = dict
+        }
+        
+        var dict: PDFDictionaryType = [:]
+        withUnsafeMutablePointer(&dict) { ctx in
+            CGPDFDictionaryApplyFunction(self, f, ctx)
+        }
+        return dict
+    }
+    
+    func dictionaryForKey(key: String) -> CGPDFDictionaryRef? {
+        return key.withCString { ckey in
+            var value = CGPDFDictionaryRef()
+            if withUnsafeMutablePointer(&value, { return CGPDFDictionaryGetDictionary(self, ckey, $0)}) {
+                return value
+            } else {
+                return nil
+            }
+        }
+    }
+}
---
+++ SwiftyPDFKit/Document.swift
@@ -0,0 +1,82 @@
+//
+//  Document.swift
+//  SwiftyPDFKit
+//
+//  Created by Kazuo Koga on 2015/12/04.
+/*
+Copyright (c) 2015 Kazuo Koga
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+import Foundation
+import CoreGraphics
+
+extension CGPDFDocumentRef {
+    
+    public var numberOfPages: Int {
+        return CGPDFDocumentGetNumberOfPages(self)
+    }
+    
+    public var catalog: PDFDictionaryType {
+        return CGPDFDocumentGetCatalog(self).shallowCopy()
+    }
+
+    public var info: PDFDictionaryType {
+        return CGPDFDocumentGetInfo(self).shallowCopy()
+    }
+
+    public var version: PDFVersion {
+        var version = PDFVersion(major: 0, minor: 0)
+        withUnsafeMutablePointers(&version.major, &version.minor) { v1, v2 in
+            CGPDFDocumentGetVersion(self, v1, v2)
+        }
+        return version
+    }
+    
+    public var identifier: (NSData, NSData) {
+        let a = CGPDFDocumentGetID(self)
+        if CGPDFArrayGetCount(a) == 2 {
+            var s1 = CGPDFStringRef()
+            var s2 = CGPDFStringRef()
+            let ok = withUnsafeMutablePointers(&s1, &s2) { v1, v2 in
+                return CGPDFArrayGetString(a, 0, v1)
+                    && CGPDFArrayGetString(a, 1, v2)
+            }
+            if ok {
+                return (NSData(bytes: CGPDFStringGetBytePtr(s1), length: CGPDFStringGetLength(s1)),
+                    NSData(bytes: CGPDFStringGetBytePtr(s2), length: CGPDFStringGetLength(s2)))
+            }
+        }
+        return (NSData(), NSData())
+    }
+    
+    public var outlines: PDFDictionaryType? {
+        let catalog = CGPDFDocumentGetCatalog(self)
+        if let ol = catalog.dictionaryForKey("Outlines") {
+            print(ol.shallowCopy())
+            let first = ol.dictionaryForKey("First")
+            print(first?.shallowCopy())
+            
+            return nil
+        } else {
+            return nil
+        }
+    }
+}
